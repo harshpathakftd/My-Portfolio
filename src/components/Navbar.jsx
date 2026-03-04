2eb5517d6f390710906098ac8c9fd1d45948bfa8
harshpathakftd <harshpathakftd@gmail.com>
2026-03-04 18:16:40 +0530

    Review section commented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = ({ navOpen }) => {
     { label: "About", link: "#about", className: "nav-link" },
     { label: "Skills", link: "#skills", className: "nav-link" },
     { label: "Work", link: "#work", className: "nav-link" },
-    { label: "Reviews", link: "#reviews", className: "nav-link" },
+    // { label: "Reviews", link: "#reviews", className: "nav-link" },
     { label: "Contact", link: "#contact", className: "nav-link md:hidden" },
   ];
 
